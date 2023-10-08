85cd79737f1302787705fb183098974467a6781d
Aditya Shantilal Bathani <95112738+Aditya4478@users.noreply.github.com>
2023-10-08 08:34:14 +0530

    Uploading FIles

---
+++ datesNotifier.js
@@ -0,0 +1,28 @@
+function checkDatesAvailability() {
+    let obj = document.querySelectorAll('.ui-datepicker-month');
+    
+    if (obj && obj[0] && obj[0][9] && obj[0][9]["attributes"] && obj[0][9]["attributes"]["1"]) {
+        let check = obj[0][9]["attributes"]["1"].value;
+        
+        if(check === 'selected') {
+            let lengthh = document.querySelectorAll('.greenday').length;
+            
+            if (lengthh !== 0) {
+                console.log("Dates available");
+
+                let audio = new Audio('https://samplelib.com/lib/preview/mp3/sample-12s.mp3');
+                audio.play();
+
+                // Stop the loop since we found available dates
+                clearInterval(intervalID);
+            } else {
+                console.log("No Dates");
+            }
+        }
+    } else {
+        console.log("Cannot find the desired object or its attributes.");
+    }
+}
+
+// Run the function every 3 seconds and store the interval ID
+let intervalID = setInterval(checkDatesAvailability, 3000);
