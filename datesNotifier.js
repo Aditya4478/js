6ff75cc560ad3e2a9ec684485cc1d6a7eb2c248c
Aditya Shantilal Bathani <95112738+Aditya4478@users.noreply.github.com>
2023-11-29 09:58:09 +0530

    Update datesNotifier.js

--- datesNotifier.js
+++ datesNotifier.js
@@ -1,31 +1,61 @@
-(function() {
-    function checkDatesAvailability() {
-        let obj = document.querySelectorAll('.ui-datepicker-month');
+function checkDatesAvailability() {
+    let obj = document.querySelectorAll('.ui-datepicker-month');
+    
+    if (obj && obj[0] && obj[0][10] && obj[0][10]["attributes"] && obj[0][10]["attributes"]["1"]) {
+        let check = obj[0][10]["attributes"]["1"].value;
         
-        if (obj && obj[0] && obj[0][10] && obj[0][10]["attributes"] && obj[0][10]["attributes"]["1"]) {
-            let check = obj[0][10]["attributes"]["1"].value;
+        if(check === 'selected') {
+            let lengthh = document.querySelectorAll('.greenday').length;
             
-            if(check === 'selected') {
-                let lengthh = document.querySelectorAll('.greenday').length;
-                
-                if (lengthh !== 0) {
-                    console.log("Dates available");
+            if (lengthh !== 0) {
+                console.log("Dates available");
 
-                    let audio = new Audio('https://aditya4478.github.io/js/music.mp3');
-                    audio.play();
-
-                    // Stop the loop since we found available dates
-                    clearInterval(intervalID);
-                } else {
-                    console.log("No Dates");
-                }
+                let audio = new Audio('https://samplelib.com/lib/preview/mp3/sample-9s.mp3');
+                audio.play();
+		clearInterval(intervalID);
+		console.log("Exiting Loop");
+            } else {
+                console.log("No Dates");
             }
-        } else {
-            console.log("Cannot find the desired object or its attributes.");
         }
+    } else if (obj && obj[0] && obj[0][11] && obj[0][11]["attributes"] && obj[0][11]["attributes"]["1"]) {
+        let check = obj[0][11]["attributes"]["1"].value;
+        
+        if(check === 'selected') {
+            let lengthh = document.querySelectorAll('.greenday').length;
+            
+            if (lengthh !== 0) {
+                console.log("Dates available");
+
+                let audio = new Audio('https://samplelib.com/lib/preview/mp3/sample-9s.mp3');
+                audio.play();
+		clearInterval(intervalID);
+		console.log("Exiting Loop");
+            } else {
+                console.log("No Dates");
+            }
+        }
+    } else {
+        console.log("Cannot find the desired object or its attributes.");
     }
+}
 
-    // Run the function every 3 seconds and store the interval ID
-    let intervalID = setInterval(checkDatesAvailability, 3000);
+// Set a timestamp for the start time
+const startTime = new Date().getTime();
 
-})();
+// Set the duration for 5 minute (300,000 milliseconds)
+const duration = 300000;
+
+// Run the function until the current time exceeds the start time + duration
+let intervalID = setInterval(function() {
+    const currentTime = new Date().getTime();
+
+    if (currentTime >= startTime + duration) {
+	let audio = new Audio('https://samplelib.com/lib/preview/mp3/sample-3s.mp3');
+        audio.play();
+        clearInterval(intervalID); // Exit the loop after 5 minute
+        console.log("Exiting the loop after 1 minute.");
+    } else {
+        checkDatesAvailability();
+    }
+}, 2000); // Run every 2 seconds
